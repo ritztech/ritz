cd2831864309c24fdd01d82f85a356dbb8830f36
ritztech <48122860+ritztech@users.noreply.github.com>
2019-03-08 12:43:25 +0530

    Add files via upload

---
+++ filehandle.py
@@ -0,0 +1,34 @@
+
+import os
+
+if os.path.exists("file555.txt"):
+  print("Yes file availabe..");
+else:
+  print("File not availabel ...");  
+
+
+
+f =  open("file1.txt");
+
+print(f.read(5));
+
+print("*************");
+
+
+print(f.readline());
+print(f.readline());
+
+
+l = open("file2.txt");
+
+
+for x in l:
+	print(x);
+	
+	
+x = open("file2.txt","a");
+
+x.write("add some more data in this file");	
+
+
+
